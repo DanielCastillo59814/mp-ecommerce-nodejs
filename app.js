a6d27df363bdd14648cac34883a7e391e0495e5a
Daniel Castillo <danielcastillo59814@ciencias,unam.mx>
2020-04-20 21:48:45 -0500

    Preferences simulation added

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const morgan = require('morgan');
 const debug = require('debug')('app');
+require('dotenv').config();
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+    sandbox: true,
+    access_token: process.env.MERCADOPAGO_ACCESS_TOKEN
+});
  
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,8 +22,17 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+app.get('/detail', async (req, res) => {
+    const preference = await mercadopago.preferences.create({
+        items: [
+            {
+                title: req.query.title,
+                unit_price: Number(req.query.price),
+                quantity: Number(req.query.unit)
+            }
+        ]
+    })
+    res.render('detail', { ...req.query, id: preference.body.id });
 });
 
 app.use(express.static('assets'));
